d373c8bafd1797999819afb7180cee2fd3e52ca5
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2024-08-05 20:06:13 -0300

    Tipos de variáveis
    atividade em python sobre tipos de variáveis

---
+++ Paulo Henrique e Saul 2024.1/Variáveis.py/tipos_de_variaveis.py
@@ -0,0 +1,15 @@
+#Variaveis numericas
+print ("Exercício 2")
+print("\n")
+
+PI = 3.14
+r = 5
+c = (4*PI)*(r**3)/3
+v = c
+print (f"o volume de uma esfera de raio 5 é igual a {v}")
+print("\n")
+print ("Exercício 3")
+
+livro = 29.45
+desconto = 0.4
+transporte = 3
