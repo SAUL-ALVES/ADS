172c75cb252b99520a4f5777b9b718475540a40f
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2025-06-11 19:57:38 -0300

    feat: adiciona telas Sobre e Professores

--- Programação Para Dispositivos Móveis/atividade02/app/src/main/java/com/example/atividade02/MainActivity.kt
+++ Programação Para Dispositivos Móveis/atividade02/app/src/main/java/com/example/atividade02/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.atividade02
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -7,16 +8,30 @@ import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.atividade02.ui.theme.Atividade02Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_times)
 
         val editTextTime = findViewById<EditText>(R.id.editTextTime)
         val buttonMostrar = findViewById<Button>(R.id.buttonMostrar)
         val buttonLimpar = findViewById<Button>(R.id.buttonLimpar)
         val textViewResultado = findViewById<TextView>(R.id.textViewResultado)
+        val buttonSobre = findViewById<Button>(R.id.buttonSobre)
+        val buttonProfessores = findViewById<Button>(R.id.buttonProfessores)
+
         val imageViewTaca = findViewById<ImageView>(R.id.imageViewTaca)
 
         imageViewTaca.visibility = View.GONE
@@ -29,14 +44,14 @@ class MainActivity : ComponentActivity() {
             "internacional" to Pair(1, R.drawable.taca_mundial),
             "corinthians" to Pair(2, R.drawable.taca_mundial)
         )
-
         buttonMostrar.setOnClickListener {
             val nomeDigitado = editTextTime.text.toString().trim().lowercase()
 
             if (mundiaisPorTime.containsKey(nomeDigitado)) {
                 val (quantidade, imagem) = mundiaisPorTime[nomeDigitado]!!
                 val nomeFormatado = nomeDigitado.replaceFirstChar { it.uppercase() }
-                textViewResultado.text = "O $nomeFormatado possui $quantidade campeonato(s) mundial(is)"
+                textViewResultado.text =
+                    "O $nomeFormatado possui $quantidade campeonato(s) mundial(is)"
                 imageViewTaca.setImageResource(imagem)
                 imageViewTaca.visibility = View.VISIBLE
             } else {
@@ -50,5 +65,16 @@ class MainActivity : ComponentActivity() {
             textViewResultado.text = ""
             imageViewTaca.visibility = View.GONE
         }
+
+        buttonSobre.setOnClickListener {
+            val intent = Intent(this, SobreActivity::class.java)
+            startActivity(intent)
+        }
+
+        buttonProfessores.setOnClickListener {
+            val intent = Intent(this, ProfessoresActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
