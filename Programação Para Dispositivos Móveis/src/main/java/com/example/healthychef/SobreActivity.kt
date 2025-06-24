c78efc7a9fda5ce1c2cc747710511bbf7435f497
SAUL ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2025-06-24 18:48:05 -0300

    projeto HealthyChef

---
+++ Programação Para Dispositivos Móveis/src/main/java/com/example/healthychef/SobreActivity.kt
@@ -0,0 +1,13 @@
+package com.example.healthychef
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class SobreActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_about)
+
+    }
+}
