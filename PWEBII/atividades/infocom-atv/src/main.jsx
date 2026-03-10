416dc97710a514f696d6aebe5684183a2c52b275
SAUL-ALVES <162472244+SAUL-ALVES@users.noreply.github.com>
2026-03-10 19:44:57 -0300

    feat: adiciona componentes e estilos para a interface do usuário

---
+++ PWEBII/atividades/infocom-atv/src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+import { CartProvider } from "./contexts/CartContext.jsx";
+import Cart from "./pages/Cart.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "products/:id",
+        element: <ProductDetails />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  </StrictMode>,
+);
---
+++ PWEBII/atividades/infocom-atv/src/pages/ProductDetails.jsx
@@ -0,0 +1,29 @@
+import { Link, useParams } from "react-router-dom";
+import { useProduct } from "../hooks/useProducts";
+import ProductCard from "../components/ProductCard";
+import "./ProductDetails.css";
+import { useCart } from "../hooks/useCart";
+import Button from "../components/Button";
+
+function ProductDetails() {
+  const { id } = useParams();
+  const { product, loading, error } = useProduct(id);
+  const { addItem } = useCart();
+
+  if (loading) return <p>Carregando detalhes do produto...</p>;
+  if (error) return <div className='error'>{error}</div>;
+  if (!product) return <p>Produto não encontrado.</p>;
+
+  return (
+    <div className="product-details">
+      <ProductCard product={product} detailedView />
+
+      <Button onClick={() => addItem(product, 1)}>Comprar</Button>
+      <Link to="/">
+        <Button variant="secondary">Início</Button>
+      </Link>
+    </div>
+  );
+}
+
+export default ProductDetails;
